c43fd1b365c4375c343e0163ba13329364de84f6
felipemendes <f.ribeiromendes@gmail.com>
2018-08-29 19:39:24 -0300

    Add script onClicked listener

---
+++ src/background/background.js
@@ -0,0 +1,20 @@
+chrome.browserAction.onClicked.addListener(function (tab) {
+
+    $(".city-placeholder .province-row label").append('<label class="checkboxLabel">Todas as cidades<input type="checkbox" class="input-checkbox"><span class="checkmark"></span></label>');
+
+    $(".province-row").each(function (index) {
+        var idCidade = "#" + $(this).attr('id');
+        $(idCidade).click(function () {
+
+            if ($(idCidade + " .input-checkbox").is(':checked')) {
+                $(idCidade + " select > option").prop("selected", "selected");
+                $(idCidade + " select").trigger("change");
+            } else {
+                $(idCidade + " select > option").removeAttr("selected");
+                $(idCidade + " select").trigger("change");
+            }
+
+        });
+
+    });
+});
